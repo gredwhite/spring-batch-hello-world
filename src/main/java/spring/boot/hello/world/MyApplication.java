f66b614a1598ac06f6621c7381eeff13c7eb3af8
ntkachev <Nikolay.Tkachev@dataart.com>
2019-08-09 12:31:00 +0300

    added spring cloud data flow(application can't start now)

--- src/main/java/spring/boot/hello/world/MyApplication.java
+++ src/main/java/spring/boot/hello/world/MyApplication.java
@@ -2,7 +2,9 @@ package spring.boot.hello.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
 
+@EnableDataFlowServer
 @SpringBootApplication
 public class MyApplication {
     public static void main(String[] args) {
